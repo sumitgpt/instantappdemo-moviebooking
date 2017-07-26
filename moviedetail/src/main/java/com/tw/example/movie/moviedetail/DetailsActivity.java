3fad5fd03ae39cb64e4948c520f9d2773e35811d
Sumit Gupta <sumitgpt@thoughtworks.com>
2017-07-26 15:13:14 +0530

    Creating instant apps

--- moviedetail/src/main/java/com/tw/example/movie/moviedetail/DetailsActivity.java
+++ moviedetail/src/main/java/com/tw/example/movie/moviedetail/DetailsActivity.java
@@ -1,6 +1,7 @@
 package com.tw.example.movie.moviedetail;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,14 +12,13 @@ import com.tw.example.movie.moviebooking.moviedetail.R;
 
 public class DetailsActivity extends Activity {
 
-    public static String NAME = "com.tw.example.movie.moviebooking.NAME";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        String name = getIntent().getStringExtra(NAME);
-        Movie movie = MovieRepository.getMovie(name);
+        Uri appLinkData = getIntent().getData();
+        String movieName = appLinkData.getQueryParameter("name");
+        Movie movie = MovieRepository.getMovie(movieName);
         ImageView imageView = (ImageView) findViewById(R.id.poster);
         imageView.setImageResource(movie.getPoster());
         TextView textView = (TextView) findViewById(R.id.desc);
--- movielisting/src/main/java/com/tw/example/movie/movielisting/ListingActivity.java
+++ movielisting/src/main/java/com/tw/example/movie/movielisting/ListingActivity.java
@@ -2,13 +2,12 @@ package com.tw.example.movie.movielisting;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.tw.example.movie.moviebooking.movielisting.R;
-import com.tw.example.movie.moviedetail.DetailsActivity;
 
 public class ListingActivity extends Activity {
 
@@ -21,8 +20,9 @@ public class ListingActivity extends Activity {
     public void openDesc(View view) {
         LinearLayout linearLayout = (LinearLayout) view;
         TextView textView = (TextView) linearLayout.getChildAt(1);
-        Intent intent = new Intent(this, DetailsActivity.class);
-        intent.putExtra(DetailsActivity.NAME, textView.getText());
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://moviebooking.com/detail?name=" + textView.getText()));
+        intent.setPackage("com.tw.example.movie.moviebooking");
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
         this.startActivity(intent);
     }
 }
